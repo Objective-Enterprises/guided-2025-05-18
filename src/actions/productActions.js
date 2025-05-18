9fb810fb5829f54a62d7e54f7b66a21ad1879a8a
David Y. Stephenson <david@davidystephenson.com>
2025-05-18 12:38:25 -0400

    Create actions

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,8 +1,20 @@
-//WRITE YOUR CODE HERE
+import { START_PRODUCT, STOP_PRODUCT, START_PRODUCTS, STOP_PRODUCTS } from "../constants/productActionConstants"
+import { GET_ALL_PRODUCTS_API } from "../constants/backend"
+import axios from 'axios'
+
 export const listProducts = () => async (dispatch) => {
-//WRITE YOUR CODE HERE
+  dispatch({ type: START_PRODUCTS })
+
+  const response = await axios.get(GET_ALL_PRODUCTS_API)
+
+  dispatch({ type: STOP_PRODUCTS, payload: response.data })
 }
 
 export const fetchProductDetails = (productId) => async (dispatch) => {
-//WRITE YOUR CODE HERE
+  dispatch({ type: START_PRODUCT })
+
+  const url = `${GET_ALL_PRODUCTS_API}/${productId}`
+  const response = await axios.get(url)
+  
+  dispatch({ type: STOP_PRODUCT, payload: response.data })
 }
